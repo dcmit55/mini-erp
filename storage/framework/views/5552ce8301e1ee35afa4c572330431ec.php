01b0174a706e6cbdc07803f776d40ab89b4b7cb8
Syifa <dcmopstech@gmail.com>
2026-02-07 12:41:29 +0700

    fix

--- storage/framework/views/5552ce8301e1ee35afa4c572330431ec.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Internal Server Error')); ?>
-<?php $__env->startSection('code', '500'); ?>
-<?php $__env->startSection('message', __('Something went wrong on our end. Our team has been notified and is working to fix this issue.')); ?>
-
-<?php echo $__env->make('errors.minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\27JAN\resources\views/errors/500.blade.php ENDPATH**/ ?>
